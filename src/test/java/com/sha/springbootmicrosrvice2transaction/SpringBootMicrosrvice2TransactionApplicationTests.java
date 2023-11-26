6f0e7d4acf09e0f845ca7511db599b7bb52c7c28
caner <66187631+cnrgrl@users.noreply.github.com>
2023-11-26 01:03:28 +0100

    Initial commit with application properties configurations

---
+++ src/test/java/com/sha/springbootmicrosrvice2transaction/SpringBootMicrosrvice2TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.springbootmicrosrvice2transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicrosrvice2TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
