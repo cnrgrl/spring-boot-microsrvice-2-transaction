c688c80c06474cc35bf4ca8c2743b53c4d959436
caner <66187631+cnrgrl@users.noreply.github.com>
2023-11-26 02:37:44 +0100

    delete and get request created

--- src/main/java/com/sha/springbootmicrosrvice2transaction/controller/TransactionController.java
+++ src/main/java/com/sha/springbootmicrosrvice2transaction/controller/TransactionController.java
@@ -5,10 +5,7 @@ import com.sha.springbootmicrosrvice2transaction.service.ITransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/transaction")
@@ -19,5 +16,16 @@ public class TransactionController {
     @PostMapping
     public ResponseEntity<?> saveTransaction(@RequestBody Transaction transaction) {
         return new ResponseEntity<>(transactionService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{transactionId}")
+    public ResponseEntity<?> deleteTransaction(@PathVariable Long transactionId) {
+        transactionService.deleteTransaction(transactionId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{userId}")
+    public ResponseEntity<?> getAllTransactionOfUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(transactionService.findAllTransactionOfUser(userId));
     }
 }
